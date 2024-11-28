eeea15d2cf6178e4c614c2629b43cb5d87d6daeb
Chinmai <chinmaidevi2000.com>
2024-11-28 17:36:26 +0530

    some changes made

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 userid=$(id -u)
+timestamp=$(date -%F+%H+%M+%S)
+scriptname=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$scriptname-$timestamp.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 .. $G Success $N"
+    else
+        echo -e "$2 .. $R Failure $N"
+        exit 1
+    fi
+}
 
 if [ $userid -eq 0 ]
     then 
@@ -9,3 +26,12 @@ else
     echo "pls run with admin access"
     exit 1
 fi
+
+dnf install mysql-server -y &>>$logfile
+validate $? "MySQL Server installation is"
+systemctl enable mysqld &>>$logfile
+validate $? "enabling MySQL Server"
+systemctl start mysqld &>>$logfile
+validate $? "Starting MySQL Server is"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
+
