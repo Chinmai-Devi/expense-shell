e74b031d3aedc07dd0bc12eef44bcb6deccd4c1d
Chinmai <chinmaidevi2000.com>
2024-11-30 15:41:00 +0530

    some changes

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+echo "pls enter DB password"
+read -s mysql_root_password 
 
 validate(){
     if [ $1 -ne 0 ]
@@ -41,10 +43,10 @@ validate $? "Starting MySQL Server is"
 
 #below command will be usefull for idempotent nature
 
-mysql -h chinmai.cloud -uroot -pExpenseApp@1 -e 'show databses;' &>>$logfile
+mysql -h chinmai.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$logfile
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$logfile
     validate $? "Setting some root password"
 else
     echo -e "Root swd is already set up so \e[33m skipping $N"
